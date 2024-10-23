80223307749ee1ca7cc83c5bb5ff8b39f6154963
Eilinafta Gita <153100710+Eilinaftagita@users.noreply.github.com>
2024-10-23 08:24:52 +0700

    Add files via upload

---
+++ fungsi_rekrusif.py
@@ -0,0 +1,11 @@
+print("----Program Hitung Keliling dan Luas Persegi Panjang----")
+print("Nama : Eilinafta Gita")
+print("Prodi : Teknik Informatika 2022")
+def Keliling(x,y):
+    return 2*(x+y)
+def Luas(x,y):
+    return x*y
+pjg=float(input("Panjang ="))
+lbr=float(input("Lebar ="))
+print("Keliling = ",Keliling(pjg,lbr))
+print("Luas = ", Luas(pjg,lbr))
